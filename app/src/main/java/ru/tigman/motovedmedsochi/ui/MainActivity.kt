c8af6a4abb4ec3dfff915e32bcdc52b6d75d8487
tigman-vip <shunik2000>
2022-04-29 01:00:11 +0700

    Очистил лишнее

--- app/src/main/java/ru/tigman/motovedmedsochi/ui/MainActivity.kt
+++ app/src/main/java/ru/tigman/motovedmedsochi/ui/MainActivity.kt
@@ -1,14 +1,18 @@
 package ru.tigman.motovedmedsochi.ui
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import com.savvi.rangedatepicker.CalendarPickerView
 import ru.tigman.motovedmedsochi.R
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-
 		setContentView(R.layout.activity_main)
 	}
 }
